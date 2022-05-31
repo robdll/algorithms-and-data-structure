51d0e92c48a4dec702df6df82b9566a7f2d43396
Roberto Di Lillo <dilillo.roberto@gmail.com>
2022-05-31 08:37:29 +0000

    Update quicksort.js

--- js/quicksort.js
+++ js/quicksort.js
@@ -1,6 +1,6 @@
 // Quicksort  
-// Time Complexity	worst case O(n^2) - avg case  O(n*log(n))	
-// Space Complexity worst case O(n) - avg case O(log n)
+// Time Complexity	    worst case O(n^2)       - avg case  O(n*log(n))	
+// Space Complexity     worst case O(n)         - avg case O(log n)
 function quicksort(arr, start, end) {
 
     console.log(`start: ${start} end: ${end}`);
