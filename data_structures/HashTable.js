9355780536457af47963945df2030105c7bfa3ff
Roberto Di Lillo <dilillo.roberto@gmail.com>
2022-06-03 12:30:36 +0000

    Create HashTable.js

---
+++ data_structures/HashTable.js
@@ -0,0 +1,46 @@
+
+const hash = (key, size) => {
+  let hashedKey = 0
+  for (let i = 0; i < key.length; i++) {
+    hashedKey += key.charCodeAt(i)
+  }
+  return hashedKey % size
+}
+
+class HashTable {
+  constructor() {
+    this.size = 20
+    this.buckets = Array(this.size) 
+
+    // populate each bucket with a Map()
+    for (let i = 0; this.buckets.length; i++) {
+      this.buckets[i] = new Map()
+    }
+  }
+
+  insert(key, value) {
+    let idx = hash(key, this.size) 
+    this.buckets[idx].set(key, value)
+  }
+
+  remove(key) {
+    let idx = hash(key, this.size)
+    let deleted = this.buckets[idx].get(key)
+    this.buckets[idx].delete(key)
+    return deleted 
+  }
+
+  search(key) {
+    let idx = hash(key, this.size)
+    return this.buckets[idx].get(key)
+  }
+}
+
+const hashTable = new HashTable()
+
+hashTable.insert('Italy', 'Rome')
+hashTable.insert('French', 'Paris')
+hashTable.insert('Spain', 'Madrid')
+
+hashTable.search('Madrid')
+hashTable.remove('Spain')
