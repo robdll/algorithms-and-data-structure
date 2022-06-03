2b4adfe48781587f9a8411322d5616812d44d7c0
Roberto Di Lillo <dilillo.roberto@gmail.com>
2022-06-03 12:41:12 +0000

    Update HashTable.js

--- data_structures/HashTable.js
+++ data_structures/HashTable.js
@@ -1,46 +1,82 @@
-
-const hash = (key, size) => {
-  let hashedKey = 0
-  for (let i = 0; i < key.length; i++) {
-    hashedKey += key.charCodeAt(i)
-  }
-  return hashedKey % size
-}
-
 class HashTable {
-  constructor() {
-    this.size = 20
-    this.buckets = Array(this.size) 
 
-    // populate each bucket with a Map()
-    for (let i = 0; this.buckets.length; i++) {
-      this.buckets[i] = new Map()
+    constructor() {
+      this.table = new Array(63);
+      this.size = 0;
+    }
+  
+    hash(key) {
+      let hash = 0;
+      for (let i = 0; i < key.length; i++) {
+        hash += key.charCodeAt(i);
+      }
+      return hash % this.table.length;
+    }
+  
+    set(key, value) {
+      const index = this.hash(key);
+      if (this.table[index]) {
+        for (let i = 0; i < this.table[index].length; i++) {
+          if (this.table[index][i][0] === key) {
+            this.table[index][i][1] = value;
+            return;
+          }
+        }
+        this.table[index].push([key, value]);
+      } else {
+        this.table[index] = [];
+        this.table[index].push([key, value]);
+      }
+      this.size++;
+    }
+  
+    get(key) {
+      const index = this.hash(key);
+      if (this.table[index]) {
+        for (let i = 0; i < this.table.length; i++) {
+          if (this.table[index][i][0] === key) {
+            return this.table[index][i][1];
+          }
+        }
+      }
+      return undefined;
+    }
+  
+    remove(key) {
+      const index = this.hash(key);
+  
+      if (this.table[index] && this.table[index].length) {
+        for (let i = 0; i < this.table.length; i++) {
+          if (this.table[index][i][0] === key) {
+            this.table[index].splice(i, 1);
+            this.size--;
+            return true;
+          }
+        }
+      } else {
+        return false;
+      }
+    }
+  
+    toString() {
+      this.table.forEach((values, index) => {
+        const chainedValues = values.map(
+          ([key, value]) => `[ ${key}: ${value} ]`
+        );
+        console.log(`${index}: ${chainedValues}`);
+      });
     }
   }
 
-  insert(key, value) {
-    let idx = hash(key, this.size) 
-    this.buckets[idx].set(key, value)
-  }
+const ht = new HashTable();
 
-  remove(key) {
-    let idx = hash(key, this.size)
-    let deleted = this.buckets[idx].get(key)
-    this.buckets[idx].delete(key)
-    return deleted 
-  }
+ht.set("France", 12);
+ht.set("Spain", 15);
 
-  search(key) {
-    let idx = hash(key, this.size)
-    return this.buckets[idx].get(key)
-  }
-}
-
-const hashTable = new HashTable()
-
-hashTable.insert('Italy', 'Rome')
-hashTable.insert('French', 'Paris')
-hashTable.insert('Spain', 'Madrid')
-
-hashTable.search('Madrid')
-hashTable.remove('Spain')
+console.log('Ht Size:', ht.size); // 3
+ht.toString();
+// 3: [ Spain: 15 ]
+// 24: [ France: 12 ]
+ht.remove("Spain");
+ht.toString();
+// 24: [ France: 12 ]
